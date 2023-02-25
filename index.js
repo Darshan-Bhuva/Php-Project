de76285248aaafc4bdf6026ef7b092f27c6770f8
Darshan-Bhuva <bhuvadr46@gmail.com>
2023-02-25 10:16:36 +0530

    Added cursor animation

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener("mousemove", (event) => {
+    setTimeout(() => {
+        outer_circle.setAttribute("style", `transform:translate(${event.clientX - 20}px,${event.clientY - 20}px)`);
+    }, 100)
+    setTimeout(() => {
+        innner_dot.setAttribute("style", `transform:translate(${event.clientX - 5}px,${event.clientY - 5}px)`);
+    }, 200)
+})
